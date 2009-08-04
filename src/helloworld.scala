b0dec6095ad515a14b85efa89debc32cefe67d7d
gerryxiao@gmail.com <gerryxiao@gmail.com>
2009-08-04 15:24:30 +0800

    first src commit

---
+++ src/helloworld.scala
@@ -0,0 +1,13 @@
+ 
+object HelloWorld extends Application {
+  println("hello world")
+}
+
+object HelloWorld2  {
+  def main(args:Array[String]) = {
+    var greeting="Hello World"
+    val name:String="gerry xiao"
+    println(greeting+name)
+  }
+}
+
